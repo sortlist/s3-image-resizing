407cfa0a55967e8ddf7827c85ff5122a890f8d75
Simon Claessens <gagalago@gmail.com>
2017-09-21 16:37:33 +0200

    keep same image type

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ const imageminSvgo = require('imagemin-svgo')
 const BUCKET = process.env.BUCKET
 const URL = process.env.URL
 
-// Imagemin options object for all image types
-const imageminOptions = {
-  optimizationLevel: +7,
-  progressive: true,
-  interlaced: true
-}
-
 exports.handler = function (event, context, callback) {
   const key = event.queryStringParameters.key
   const match = key.match(/(\d+)x(\d+)\/(.*)/)
@@ -29,21 +22,20 @@ exports.handler = function (event, context, callback) {
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
-      .toFormat('png')
       .toBuffer()
+      .then(buffer => imagemin.buffer(buffer, { plugins: [
+        imageminJpegtran(),
+        imageminOptipng(),
+        imageminGifsiclet(),
+        imageminSvgo()
+      ]}))
+      .then(buffer => S3.putObject({
+        Body: buffer,
+        Bucket: BUCKET,
+        ContentType: data.ContentType,
+        Key: key
+      }).promise())
     )
-    .then(buffer => imagemin.buffer(buffer, { plugins: [
-      imageminJpegtran(),
-      imageminOptipng(),
-      imageminGifsiclet(),
-      imageminSvgo()
-    ]}))
-    .then(buffer => S3.putObject({
-      Body: buffer,
-      Bucket: BUCKET,
-      ContentType: 'image/png',
-      Key: key
-    }).promise())
     .then(() => callback(null, {
       statusCode: '301',
       headers: {'location': `${URL}/${key}`},
